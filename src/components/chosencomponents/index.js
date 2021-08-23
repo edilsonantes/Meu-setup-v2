821b808cb224aa6b3428dafa4aab889ee7ce9300
edilson <nantes.junior@gmail.com>
2021-08-23 18:31:44 -0300

    Implementação das actions para manipular a configuração (foram reutilizadas da versão mobile da aplicação)

--- src/components/chosencomponents/index.js
+++ src/components/chosencomponents/index.js
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import './chosencomponents.css';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 function ChosenComponents(){
 
     function Teste(){
         console.log('TESTANDO')
     }
+
+    var config = useSelector(state => state.config)
 
     return (
         <div className="col-md-4 justify-content-center col-sm-6">
@@ -15,19 +18,19 @@ function ChosenComponents(){
                 <input type="text" name="nome" className="campo mb-3" id="nome"/>
             </div>                        
             <div onClick={Teste} id="procEscolhido" className="row componenteEscolhido my-2">
-                <Link className="text-decoration-none"><p>Processador</p></Link>
+                <Link className="text-decoration-none"><p>{config.cpu.nome}</p></Link>
             </div>
             <div onClick={Teste} id="procEscolhido" className="row componenteEscolhido my-2">
-                <Link className="text-decoration-none"><p>Placa-mãe</p></Link>
+                <Link className="text-decoration-none"><p>{config.mb.nome}</p></Link>
             </div>
             <div onClick={Teste} id="procEscolhido" className="row componenteEscolhido my-2">
-                <Link className="text-decoration-none"><p>Memória RAM</p></Link>
+                <Link className="text-decoration-none"><p>{config.ram.nome}</p></Link>
             </div>
             <div onClick={Teste} id="procEscolhido" className="row componenteEscolhido my-2">
-                <Link className="text-decoration-none"><p>Placa de Vídeo</p></Link>
+                <Link className="text-decoration-none"><p>{config.gpu.nome}</p></Link>
             </div>
             <div onClick={Teste} id="procEscolhido" className="row componenteEscolhido my-2">
-                <Link className="text-decoration-none"><p>Fonte</p></Link>
+                <Link className="text-decoration-none"><p>{config.pwr.nome}</p></Link>
             </div>
             <div className="row my-2 justify-content-center">
                 <input type="button" value="Salvar" className="btn btn-warning salvar" id="salvar"/>
--- src/reducers/configReducer.js
+++ src/reducers/configReducer.js
@@ -1,14 +1,13 @@
 import {useSelector} from 'react-redux';
-import {SET_EMAIL} from '../actions';
+import { SET_CPU, SET_GPU, SET_MB, SET_PWR, SET_RAM } from "../actions";
 
 const INITIAL_STATE = {
-    usuarioEmail: '',
     tipoComponent: 'cpu',
-    cpu: '',
-    mb: '',
-    ram: '',
-    gpu: '',
-    pwr: ''
+    cpu: {nome: 'Processador', soq: null},
+    mb: {nome: 'Placa-mãe', soq: null, vel: null},
+    ram: {nome: 'Memória RAM', vel: null},
+    gpu: {nome: 'Placa de Vídeo', pwr: null},
+    pwr: {nome: 'Fonte', pot: null}
 };
 
 
@@ -17,8 +16,34 @@ function configReducer(state = INITIAL_STATE, action){
     //console.log(state);
     
     switch (action){
-        case SET_EMAIL:
-            return {...state, usuarioEmail: action.email};
+        case SET_CPU:
+            state.CPU = action.data;
+            if (state.CPU.soq != state.MB.soq){
+                state.MB.MB = 'Placa-mãe';
+                state.MB.vel = null;
+                state.MB.soq = null;
+            }
+            return state;
+        case SET_MB:
+            state.MB = action.data;
+            if(state.MB.vel < state.RAM.vel){
+                state.RAM.RAM = 'Memória RAM';
+                state.RAM.vel = null;
+            }
+            return state;
+        case SET_RAM:
+            state.RAM = action.data;
+            return state;
+        case SET_GPU:
+            state.GPU = action.data;
+            if(state.GPU.pwr > state.PWR.pot){
+                state.PWR.PWR = 'Fonte';
+                state.PWR.pot = null;
+            }
+            return state;
+        case SET_PWR:
+            state.PWR = action.data;
+            return state;
         default:
             return state;
     }
